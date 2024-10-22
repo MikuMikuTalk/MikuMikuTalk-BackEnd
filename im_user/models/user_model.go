bcfa3a907abf53121018e1479ccf846446d48631
meowrain <meowrain@126.com>
2024-10-22 11:26:33 +0800

    init: 项目初始化

--- im_user/models/user_model.go
+++ im_user/models/user_model.go
@@ -4,4 +4,10 @@ import "im_server/common/models"
 
 type UserModel struct {
 	models.Models
+	Pwd      string `gorm:"size:64" json:"pwd"`
+	Nickname string `gorm:"size:32" json:"nickname"`
+	Abstract string `gorm:"size:128" json:"abstract"`
+	Avatar   string `gorm:"size:256" json:"avatar"`
+	IP       string `gorm:"size:32" json:"ip"`
+	Addr     string `gorm:"size:64" json:"addr"`
 }
