5e0a848fb397685cbc038d86dd803692dc6f347b
meowrain <meowrain@126.com>
2024-10-25 14:09:27 +0800

    fix: 修复swagger无法请求的问题

--- im_auth/auth_api/auth.go
+++ im_auth/auth_api/auth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 
 	"im_server/im_auth/auth_api/internal/config"
 	"im_server/im_auth/auth_api/internal/handler"
@@ -20,7 +21,11 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithCustomCors(func(header http.Header) {
+		header.Set("Access-Control-Allow-Origin", "*")
+		header.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+		header.Set("Access-Control-Expose-Headers", "Content-Length, Content-Type")
+	}, nil, "*"))
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
