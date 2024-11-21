a50bd41ff0c48ef970505fa75b266e76d4f3b429
meowrain <meowrain@126.com>
2024-11-21 19:20:10 +0800

    feat: 添加正则表达式白名单解析，添加获取图片路径到白名单

--- im_auth/auth_api/internal/logic/authenticationlogic.go
+++ im_auth/auth_api/internal/logic/authenticationlogic.go
@@ -28,18 +28,24 @@ func NewAuthenticationLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Au
 }
 
 func (l *AuthenticationLogic) Authentication(req *types.AuthenticationRequest) (resp string, err error) {
-	if whitelist.IsInList(req.ValidPath, l.svcCtx.Config.WhiteList) {
-		return "ok", nil
+	// if whitelist.IsInList(req.ValidPath, l.svcCtx.Config.WhiteList) {
+	// 	return "ok", nil
+	// }
+	//使用正则表达式
+	if whitelist.IsInListByRegex(l.svcCtx.Config.WhiteList, req.ValidPath) {
+		logx.Infof("%s 在白名单中", req.ValidPath)
+		return
 	}
 	// 检查是否提供了 Authorization
 	if req.Authorization == "" {
+		logx.Error("Authorization 请求头为空")
 		return "", errors.New("认证失败：缺少授权信息")
 	}
 	// 验证Token是否有效
 	isValid, err := jwts.ValidateToken(req.Authorization, l.svcCtx.Config.Auth.AuthSecret, l.svcCtx.RDB)
 	if err != nil || !isValid {
-		err = errors.New("认证失败")
-		return
+		return "", errors.New("认证失败")
 	}
+
 	return "ok", nil
 }
--- im_auth/whitelist/enter.go
+++ im_auth/whitelist/enter.go
@@ -1,8 +1,27 @@
 package whitelist
 
+import (
+	"regexp"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
 func IsInList(path string, whitelist []string) bool {
 	for _, v := range whitelist {
 		if path == v {
+			return true
+		}
+	}
+	return false
+}
+func IsInListByRegex(list []string, key string) bool {
+	for _, s := range list {
+		regex, err := regexp.Compile(s)
+		if err != nil {
+			logx.Error(err)
+			return false
+		}
+		if regex.MatchString(key) {
 			return true
 		}
 	}
