8d56b696e4a29d840d2de52aa1ef079ad324c483
meowrain <meowrain@126.com>
2024-11-25 22:49:02 +0800

    feat: list_query支持连接查询

--- common/list_query/enter.go
+++ common/list_query/enter.go
@@ -9,18 +9,23 @@ import (
 
 type Option struct {
 	PageInfo models.PageInfo
-	Where    *gorm.DB //高级查询
-	Likes    []string // 模糊匹配得字段
-	Preload  []string //预加载字段
+	Where    *gorm.DB // 高级查询
+	Joins    string
+	Likes    []string // 模糊匹配的字段
+	Preload  []string // 预加载字段
 }
 
 func ListQuery[T any](db *gorm.DB, model T, option Option) (list []T, count int64, err error) {
+
+	// 把结构体自己的查询条件查了
 	query := db.Where(model)
 
+	// 模糊匹配
 	if option.PageInfo.Key != "" && len(option.Likes) > 0 {
 		likeQuery := db.Where("")
 		for index, column := range option.Likes {
 			if index == 0 {
+				// where name like '%fengfeng%'
 				likeQuery.Where(fmt.Sprintf("%s like '%%?%%'", column), option.PageInfo.Key)
 			} else {
 				likeQuery.Or(fmt.Sprintf("%s like '%%?%%'", column), option.PageInfo.Key)
@@ -28,6 +33,15 @@ func ListQuery[T any](db *gorm.DB, model T, option Option) (list []T, count int6
 		}
 		query.Where(likeQuery)
 	}
+
+	if option.Joins != "" {
+		query = query.Joins(option.Joins)
+	}
+
+	if option.Where != nil {
+		query = query.Where(option.Where)
+	}
+
 	// 求总数
 	query.Model(model).Count(&count)
 
