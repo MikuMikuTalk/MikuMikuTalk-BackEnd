d7c63af72390f2212162360842b68a89a6c85443
meowrain <meowrain@126.com>
2024-12-14 12:39:18 +0800

    fix: 修复用户名和原来一样无法更新用户信息的问题

--- im_user/user_api/internal/logic/userinfoupdatelogic.go
+++ im_user/user_api/internal/logic/userinfoupdatelogic.go
@@ -32,44 +32,55 @@ func NewUserInfoUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Us
 }
 
 func (l *UserInfoUpdateLogic) UserInfoUpdate(token string, req *types.UserInfoUpdateRequest) (resp *types.UserInfoUpdateResponse, err error) {
+	//获取claims
 	claims, err := jwts.ParseToken(token, l.svcCtx.Config.Auth.AuthSecret)
 	if err != nil {
 		logx.Error("error: ", err)
 		return nil, err
 	}
+	//获取请求的用户的id
 	user_id := claims.UserID
 	userMaps := ref_map.RefToMap(*req, "user")
 	userConfMaps := ref_map.RefToMap(*req, "user_conf")
 	logx.Info("userMaps: ", userMaps)
 	logx.Info("userConfMaps:", userConfMaps)
+	// 检查 `userMaps` 是否有内容
 	if len(userMaps) != 0 {
 		var user user_models.UserModel
+		// 查询请求的用户信息
 		err = l.svcCtx.DB.Take(&user, user_id).Error
 		if err != nil {
 			return nil, errors.New("用户不存在")
 		}
 
-		// 防止用户重名，优化重名baocuo
-		if nick, ok := userMaps["nickname"].(string); ok && nick != user.Nickname {
-			var existingUser user_models.UserModel
-			err = l.svcCtx.DB.Where("nickname = ?", nick).First(&existingUser).Error
-			if err == nil {
-				return nil, errors.New("用户名已存在")
-			} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-				logx.Error("检查用户名是否存在时发生错误: ", err)
-				return nil, errors.New("用户信息更新失败")
+		// 判断是否需要更新 `nickname`
+		if nick, ok := userMaps["nickname"].(string); ok {
+			//从发过来的信息中提取用户信息，判断是否需要更新昵称
+			if nick != user.Nickname {
+				// 如果新昵称和原昵称不同，检查是否重复
+				var existingUser user_models.UserModel
+				err = l.svcCtx.DB.Where("nickname = ?", nick).First(&existingUser).Error
+				if err == nil {
+					return nil, errors.New("用户名已存在")
+				} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+					logx.Error("检查用户名是否存在时发生错误: ", err)
+					return nil, errors.New("用户信息更新失败")
+				}
+			} else {
+				// 如果昵称未改变，移除更新逻辑中的 `nickname`
+				delete(userMaps, "nickname")
 			}
 		}
 
-		if *(req.Nickname) == user.Nickname {
-			err = errors.New("已经是原来的用户名了哦")
-			return nil, err
-		}
-		err = l.svcCtx.DB.Model(&user).Updates(userMaps).Error
-		if err != nil {
-			return nil, errors.New("用户信息更新失败")
+		// 更新用户其他字段
+		if len(userMaps) > 0 {
+			err = l.svcCtx.DB.Model(&user).Updates(userMaps).Error
+			if err != nil {
+				return nil, errors.New("用户信息更新失败")
+			}
 		}
 	}
+
 	if len(userConfMaps) != 0 {
 		var userConf user_models.UserConfModel
 		err = l.svcCtx.DB.Take(&userConf, "user_id = ?", user_id).Error
