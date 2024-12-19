e9eabf088d87fa8dc99e7176c5aa580980aa0754
meowrain <meowrain@126.com>
2024-12-19 11:12:50 +0000

    feat: 请求聊天时候判断好友是否在线

--- im_chat/chat_api/internal/handler/chatwebsocketconnectionhandler.go
+++ im_chat/chat_api/internal/handler/chatwebsocketconnectionhandler.go
@@ -2,13 +2,16 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"im_server/common/response"
 	"im_server/im_chat/chat_api/internal/svc"
 	"im_server/im_chat/chat_api/internal/types"
+	"im_server/im_user/user_models"
 	"im_server/im_user/user_rpc/types/user_rpc"
+	"im_server/utils/jwts"
 
 	"github.com/gorilla/websocket"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -36,7 +39,13 @@ func chatWebsocketConnectionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc
 			response.Response(r, w, nil, err)
 			return
 		}
-
+		token := r.Header.Get("Authorization")
+		claims, err := jwts.ParseToken(token, svcCtx.Config.Auth.AuthSecret)
+		if err != nil {
+			response.Response(r, w, nil, err)
+			return
+		}
+		my_id := claims.UserID
 		upGrader := websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
 				// 鉴权 true表示放行，false表示拦截
@@ -54,10 +63,17 @@ func chatWebsocketConnectionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc
 			conn.Close()
 			delete(UserWsMap, req.UserID)
 		}()
-		// 调取用户服务，获取当前用户信息
-		res, err := svcCtx.UserRpc.UserListInfo(context.Background(), &user_rpc.UserListInfoRequest{
-			UserIdList: []uint32{uint32(req.UserID)},
+		// 调取用户服务，获取好友信息
+		res, err := svcCtx.UserRpc.UserInfo(context.Background(), &user_rpc.UserInfoRequest{
+			UserId: uint32(req.UserID),
 		})
+		if err != nil {
+			logx.Error(err)
+			response.Response(r, w, nil, err)
+			return
+		}
+		var userInfo user_models.UserModel
+		err = json.Unmarshal(res.Data, &userInfo)
 		if err != nil {
 			logx.Error(err)
 			response.Response(r, w, nil, err)
@@ -66,15 +82,45 @@ func chatWebsocketConnectionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc
 		var userWsInfo = UserWsInfo{
 			UserInfo: UserInfo{
 				UserID:   req.UserID,
-				Avatar:   res.UserInfo[uint32(req.UserID)].Avatar,
-				NickName: res.UserInfo[uint32(req.UserID)].NickName,
+				Avatar:   userInfo.Avatar,
+				NickName: userInfo.Nickname,
 			},
 			Conn: conn,
 		}
 		UserWsMap[req.UserID] = userWsInfo
 		logx.Info("UserWsMap: ", UserWsMap)
 		logx.Info("userWsInfo: ", userWsInfo)
+		/*
+		   // 遍历在线的用户， 和当前这个人是好友的，就给他发送好友在线
 
+		   		// 先把所有在线的用户id取出来，以及待确认的用户id，然后传到用户rpc服务中
+		   		// [1,2,3]  3
+		   		// 在rpc服务中，去判断哪些用户是好友关系
+
+		   		//if userInfo.UserConfModel.FriendOnline {
+		   		// 如果用户开启了好友上线提醒
+		   		// 查一下自己的好友是不是上线了
+
+		*/
+
+		friendRes, err := svcCtx.UserRpc.FriendList(context.Background(), &user_rpc.FriendListRequest{
+			User: uint32(my_id),
+		})
+		if err != nil {
+			logx.Error(err)
+			response.Response(r, w, nil, err)
+			return
+		}
+
+		for _, info := range friendRes.FriendList {
+			logx.Info(info)
+			friend, ok := UserWsMap[uint(info.UserId)]
+			if ok {
+				text := fmt.Sprintf("好友 %s 上线了", UserWsMap[req.UserID].UserInfo.NickName)
+				logx.Info(text)
+				friend.Conn.WriteMessage(websocket.TextMessage, []byte("好友上线了"))
+			}
+		}
 		for {
 			// 消息类型，消息，错误
 			_, p, err := conn.ReadMessage()
---
+++ im_user/user_rpc/internal/logic/friendlistlogic.go
@@ -0,0 +1,58 @@
+package logic
+
+import (
+	"context"
+
+	"im_server/common/list_query"
+	"im_server/common/models"
+	"im_server/im_user/user_models"
+	"im_server/im_user/user_rpc/internal/svc"
+	"im_server/im_user/user_rpc/types/user_rpc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type FriendListLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewFriendListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FriendListLogic {
+	return &FriendListLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *FriendListLogic) FriendList(in *user_rpc.FriendListRequest) (*user_rpc.FriendListResponse, error) {
+	friends, _, _ := list_query.ListQuery(l.svcCtx.DB, user_models.FriendModel{}, list_query.Option{
+		PageInfo: models.PageInfo{
+			Limit: -1, //查看全部
+		},
+		Preload: []string{"SendUserModel", "RevUserModel"},
+	})
+	var list []*user_rpc.FriendInfo
+	for _, friend := range friends {
+		info := user_rpc.FriendInfo{}
+		if friend.SendUserID == uint(in.User) {
+			//我是发起方
+			info.UserId = uint32(friend.RevUserID)
+			info.NickName = friend.RevUserModel.Nickname
+			info.Avatar = friend.RevUserModel.Avatar
+		}
+
+		if friend.RevUserID == uint(in.User) {
+			//如果我是接收方
+			info.UserId = uint32(friend.SendUserID)
+			info.NickName = friend.SendUserModel.Nickname
+			info.Avatar = friend.SendUserModel.Avatar
+		}
+		list = append(list, &info)
+	}
+
+	return &user_rpc.FriendListResponse{
+		FriendList: list,
+	}, nil
+}
