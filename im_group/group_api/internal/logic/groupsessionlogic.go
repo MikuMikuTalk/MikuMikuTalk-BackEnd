f995b50aaf212b7819325156e146a9054c7c4a96
meowrain <meowrain@126.com>
2025-01-26 13:42:57 +0800

    feat: 用户置顶的群聊会话列表查询

--- im_group/group_api/internal/logic/groupsessionlogic.go
+++ im_group/group_api/internal/logic/groupsessionlogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"fmt"
 
 	"im_server/common/list_query"
 	"im_server/common/models"
@@ -31,6 +32,7 @@ type SessionData struct {
 	GroupID       uint   `gorm:"column:group_id"`
 	NewMsgDate    string `gorm:"column:newMsgDate"`
 	NewMsgPreview string `gorm:"column:newMsgPreview"`
+	IsTop         bool   `gorm:"column:isTop"`
 }
 
 func (l *GroupSessionLogic) GroupSession(req *types.GroupSessionRequest) (resp *types.GroupSessionListResponse, err error) {
@@ -45,22 +47,29 @@ func (l *GroupSessionLogic) GroupSession(req *types.GroupSessionRequest) (resp *
 	l.svcCtx.DB.Model(group_models.GroupMemberModel{}).
 		Where("user_id = ?", myID).
 		Select("group_id").Scan(&userGroupIDList)
+	column := fmt.Sprintf(" (if((select 1 from group_user_top_models where user_id = %d and group_user_top_models.group_id = group_msg_models.group_id), 1, 0)) as isTop", myID)
+
+	// 查哪些聊天记录是被删掉的
+	var msgDeleteIDList []uint
+	l.svcCtx.DB.Model(group_models.GroupUserMsgDeleteModel{}).Where("group_id in ?", userGroupIDList).Select("msg_id").Scan(&msgDeleteIDList)
 
 	sessionList, count, _ := list_query.ListQuery(l.svcCtx.DB, SessionData{}, list_query.Option{
 		PageInfo: models.PageInfo{
 			Page:  req.Page,
 			Limit: req.Limit,
-			Sort:  "newMsgDate desc",
+			Sort:  "isTop desc, newMsgDate desc",
 		},
 		Table: func() (string, any) {
 			return "(?) as u", l.svcCtx.DB.Model(&group_models.GroupMsgModel{}).
 				Select("group_id",
 					"max(created_at) as newMsgDate",
+					column,
 					"(select msg_preview from group_msg_models as g where g.group_id = group_id order by g.created_at desc limit 1)  as newMsgPreview").
-				Where("group_id in (?)", userGroupIDList).
+				Where("group_id in (?) and id not in ?", userGroupIDList, msgDeleteIDList).
 				Group("group_id")
 		},
 	})
+
 	var groupIDList []uint
 	for _, data := range sessionList {
 		groupIDList = append(groupIDList, data.GroupID)
@@ -79,6 +88,7 @@ func (l *GroupSessionLogic) GroupSession(req *types.GroupSessionRequest) (resp *
 			Avatar:        groupMap[data.GroupID].Avatar,
 			NewMsgDate:    data.NewMsgDate,
 			NewMsgPreview: data.NewMsgPreview,
+			IsTop:         data.IsTop,
 		})
 	}
 	resp.Count = int(count)
