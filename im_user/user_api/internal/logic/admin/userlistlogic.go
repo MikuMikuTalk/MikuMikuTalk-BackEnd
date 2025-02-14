4a453cf9c3448ea1f4078955d0a9c451ede0848b
meowrain <meowrain@126.com>
2025-02-14 22:04:01 +0800

    feat: 	用户列表

--- im_user/user_api/internal/logic/admin/userlistlogic.go
+++ im_user/user_api/internal/logic/admin/userlistlogic.go
@@ -3,8 +3,12 @@ package admin
 import (
 	"context"
 
+	"im_server/common/list_query"
+	"im_server/common/models"
 	"im_server/im_user/user_api/internal/svc"
 	"im_server/im_user/user_api/internal/types"
+	"im_server/im_user/user_models"
+	"im_server/im_user/user_rpc/types/user_rpc"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +28,44 @@ func NewUserListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserList
 }
 
 func (l *UserListLogic) UserList(req *types.UserListRequest) (resp *types.UserListResponse, err error) {
-	// todo: add your logic here and delete this line
-
+	list, count, _ := list_query.ListQuery(l.svcCtx.DB, user_models.UserModel{}, list_query.Option{
+		PageInfo: models.PageInfo{
+			Limit: req.Limit,
+			Page:  req.Page,
+			Key:   req.Key,
+		},
+		Likes: []string{"nickname", "ip"},
+	})
+	resp = new(types.UserListResponse)
+	var userIDList []uint32
+	for _, model := range list {
+		userIDList = append(userIDList, uint32(model.ID))
+	}
+	// 去查用户在线状态
+	var userOnlineMap = map[uint]bool{}
+	userOnlineResponse, err := l.svcCtx.UserRpc.UserOnlineList(l.ctx, &user_rpc.UserOnlineListRequest{})
+	if err == nil {
+		for _, u := range userOnlineResponse.UserIdList {
+			userOnlineMap[uint(u)] = true
+		}
+	} else {
+		logx.Error(err)
+	}
+	// 查用户创建的群聊个数
+	// 查用户加入的群聊个数
+	// 查用户发送的消息个数
+	for _, model := range list {
+		info := types.UserListInfoResponse{
+			ID:        model.ID,
+			CreatedAt: model.CreatedAt.String(),
+			Nickname:  model.Nickname,
+			Avatar:    model.Avatar,
+			IP:        model.IP,
+			Addr:      model.Addr,
+			IsOnline:  userOnlineMap[model.ID],
+		}
+		resp.List = append(resp.List, info)
+	}
+	resp.Count = count
 	return
 }
